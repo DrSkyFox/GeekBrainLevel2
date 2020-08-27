f643a0c8422702125b5610d93db7817a9c37e1bb
Emil <darksky270990@gmail.com>
2020-08-27 21:58:49 +0300

    Exception

---
+++ src/LessonTwo/MainExcepApp.java
@@ -0,0 +1,53 @@
+package LessonTwo;
+
+public class MainExcepApp {
+/*
+1. Напишите метод, на вход которого подаётся двумерный строковый массив размером 4х4,
+ при подаче массива другого размера необходимо бросить исключение MyArraySizeException.
+2. Далее метод должен пройтись по всем элементам массива,
+преобразовать в int, и просуммировать.
+Если в каком-то элементе массива преобразование не удалось (например, в ячейке лежит символ или текст вместо числа),
+должно быть брошено исключение MyArrayDataException, с детализацией в какой именно ячейке лежат неверные данные.
+3. В методе main() вызвать полученный метод, обработать возможные исключения MySizeArrayException и MyArrayDataException,
+ и вывести результат расчета.
+ */
+    public static void main(String[] args) {
+        System.out.println("RunProg");
+        Matrix matrix = new Matrix();
+        matrix.printMatrix(matrix.getMatrix());
+        System.out.println(matrix.isSquare());
+
+        String[][] chMattix = new String[][] {
+                {"1", "2", "3", "4"},
+                {"5", "f", "6", "7"},
+                {"8", "9", "10", "10"},
+                {"12", "13", "15" , "16"},
+                {"22", "33", "55" , "66"}
+        };
+        try {
+            matrix.setMatrix(chMattix);
+            matrix.printMatrix(matrix.getMatrix());
+
+        } catch (MyArraySizeException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+            matrix.setMatrix(new String[][] {
+                    {"1", "2", "3", "4"},
+                    {"5", "45", "6", "7"},
+                    {"8", "9", "10", "10"},
+                    {"12", "13", "15" , "f"}
+            });
+        }
+
+        try {
+            matrix.printMatrix(matrix.setStringMatrixToIntMatrix());
+        } catch (MyArrayDataException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+
+        System.out.println("Sum: " + matrix.getSum());
+
+
+    }
+}
---
+++ src/LessonTwo/MyArrayDataException.java
@@ -0,0 +1,12 @@
+package LessonTwo;
+
+public class MyArrayDataException extends Exception{
+
+    public MyArrayDataException(String message) {
+        super(message);
+    }
+
+    public MyArrayDataException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
