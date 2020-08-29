9cb7923576ec7954a4ec0300171d8680356f90ce
Emil <darksky270990@gmail.com>
2020-08-30 00:56:35 +0300

    List of Random Words and counting mechanism

--- src/LessonThree/MainCollectionApp.java
+++ src/LessonThree/MainCollectionApp.java
@@ -2,10 +2,28 @@ package LessonThree;
 
 public class MainCollectionApp {
     public static void main(String[] args) {
+        collectionTask();
+
+        phoneTask();
+    }
+
+    private static void collectionTask() {
         ListOfWord word = new ListOfWord();
         word.printList(word.getSomeWords());
         System.out.println("List of Unique");
         word.printList(word.getUniqueWordList());
         word.countingDuplicateWords(word.getUniqueWordList());
     }
+
+    private static void phoneTask() {
+        PhoneCatalog phoneCatalog = new PhoneCatalog();
+        phoneCatalog.addNewPhoneNumber("Rose", 755567);
+        phoneCatalog.addNewPhoneNumber("Joker", 45674135);
+        phoneCatalog.addNewPhoneNumber("Batman", 911);
+        phoneCatalog.addNewPhoneNumber("Rose", 123456);
+        phoneCatalog.addNewPhoneNumber("Robin", 41842398);
+        phoneCatalog.addNewPhoneNumber("Batman", 112);
+        phoneCatalog.printPhones("Rose");
+    }
+
 }
